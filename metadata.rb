4dbbe2d193967f325d2d28a61c2b2b8baaece70e
Jamie Winsor <jamie@vialstudios.com>
2013-08-27 16:30:25 -0700

    version bump 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'jamie@vialstudios.com'
 license          'Apache 2.0'
 description      'A Library Cookbook for interacting with Github'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
