aa4fdb59e2c714a3cb7830d3191e267d893a673c
John Manero <John_Manero@rapid7.com>
2014-09-22 17:23:13 -0400

    Fixing up the github_archive resource
    * Brings dependency/compatability into sync with asset. While the provider doesn't use the libarchive_file resource,
      it now relies on recipe[libarchive::default] to set up underlying packages/gems. This allows the resource to
      converge on Ubuntu/trusty, where `libarchive12` has been `libarchive13`.
    * Fixes logic bugs where :force == false would always keep the archive from being downloaded and exported
      (https://github.com/reset/github-cookbook/blob/v0.3.0/providers/archive.rb#L27)

--- README.md
+++ README.md
@@ -24,6 +24,24 @@ Downloads an asset from a Github release
 - **group** - group of the downloaded asset on disk
 - **force** - force downloading even if the asset already exists on disk
 
+## github_archive LWRP
+Downloads and extracts a tarball of a specified `ref`
+
+### Actions
+
+- **download** - downloads a tarball into the chef file-cache
+- **extract** - extracts the downloaded tarball and strips the redundant root directory
+
+### Parameter Attributes
+
+- **name** - fully qualified repository name (`org/repo`)
+- **version** - git ref/tag/HEAD to fetch. Default `master`
+- **github_user** - GitHub user
+- **github_token** - GitHub API token
+- **host** - GitHub:Enterprise endpoint. Default `https://api.github.com`
+- **extract_to** - target location for extracted source
+- **force** - override idempotent existance checks
+
 ## HTTP proxy support
 
 Ensure the `HTTPS_PROXY` environment variable is set for the shell executing `chef-client` or `chef-solo`. The value should be a fully qualified URL containing the host, port, username, and password for your proxy.
@@ -31,3 +49,6 @@ Ensure the `HTTPS_PROXY` environment variable is set for the shell executing `ch
 # Author
 
 Author:: Jamie Winsor (<jamie@vialstudios.com>)
+
+# Contributors
+- John Manero (john_manero@rapid7.com)
--- resources/archive.rb
+++ resources/archive.rb
@@ -3,17 +3,99 @@
 # Resource:: archive
 #
 # Author:: Jamie Winsor (<jamie@vialstudios.com>)
+# Contributor:: John Manero (<john_manero@rapid7.com>)
 #
 
-actions :extract, :delete
-default_action :extract
+actions :download, :extract, :delete
+default_action [:download, :extract]
 
-attribute :repo, kind_of: String, name_attribute: true, required: true
-attribute :version, kind_of: String, default: "master"
+attribute :name, kind_of: String, name_attribute: true, required: true
+attribute :version, kind_of: String, default: 'master'
 attribute :github_user, kind_of: String
 attribute :github_token, kind_of: String
-attribute :host, kind_of: String
-attribute :owner, kind_of: String
-attribute :group, kind_of: String
-attribute :extract_to, kind_of: String, required: true
+attribute :host, kind_of: String, default: 'https://api.github.com'
+attribute :extract_to, kind_of: [String, FalseClass], default: false
 attribute :force, kind_of: [TrueClass, FalseClass], default: false
+
+attr_reader :organization
+attr_reader :repo
+
+require 'net/http'
+require 'uri'
+require 'fileutils'
+
+def cache_path
+  ::File.join(Chef::Config[:file_cache_path], 'github/archives')
+end
+
+def archive_path
+  ::File.join(cache_path, "#{ organization }-#{ repo }-#{ version }.tar.gz")
+end
+
+def archive_url
+  Octokit.archive_link(name, ref: version)
+rescue Octokit::ClientError => e
+  case e
+  when Octokit::NotAcceptable
+    raise GithubCB::AuthenticationError
+  when Octokit::NotFound
+    raise GithubCB::AssetNotFound
+  else
+    raise e
+  end
+end
+
+def initialize(name, run_context)
+  super(name, run_context)
+  @organization, @repo = name.split('/')
+end
+
+def authenticate
+  require 'octokit'
+  Octokit.configure do |c|
+    c.login        = github_user
+    c.access_token = github_token
+  end
+end
+
+def download
+  FileUtils.rm_rf(options[:path]) if force
+  FileUtils.mkdir_p(::File.dirname(archive_path))
+  archive_fh = ::File.open(archive_path, 'wb')
+
+  open(archive_url) { |source| IO.copy_stream(source, archive_fh) }
+rescue Octokit::ClientError => e
+  case e
+  when Octokit::NotAcceptable
+    raise GithubCB::AuthenticationError
+  when Octokit::NotFound
+    raise GithubCB::AssetNotFound
+  else
+    raise e
+  end
+ensure
+  archive_fh.close unless archive_fh.nil?
+end
+
+def extract
+  require 'archive'
+  FileUtils.rm_rf(extract_to) if force
+
+  Dir.mktmpdir do |tmpdir|
+    Dir.chdir(tmpdir) do
+      ::Archive.new(archive_path).extract
+      ::File.rename(Dir.glob("#{ tmpdir }/**").first, extract_to)
+    end
+  end
+rescue => e
+  FileUtils.rm_rf(extract_to) if ::File.exists?(extract_to)
+  raise e
+end
+
+def downloaded?
+  ::File.exists?(archive_path)
+end
+
+def extracted?
+  ::File.exist?(extract_to)
+end
