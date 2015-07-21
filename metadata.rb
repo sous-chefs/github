c9404660c835625ef28c435ba6416f0a5177ebea
Jamie Winsor <jamie@vialstudios.com>
2015-07-21 14:06:31 -0700

    version bump 0.3.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jamie@vialstudios.com"
 license          "Apache 2.0"
 description      "A Library Cookbook for interacting with the Github API"
 long_description "A Library Cookbook for interacting with the Github API"
-version          "0.3.0"
+version          "0.3.1"
 
 supports "ubuntu"
 
