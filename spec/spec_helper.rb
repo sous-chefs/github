87a2c880230864a23e74ce901e9de475b5ef41c5
Lance Albertson <lance@osuosl.org>
2021-09-01 22:59:55 -0700

    Comment out broken ChefSpec
    Signed-off-by: Lance Albertson <lance@osuosl.org>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,37 +1,37 @@
-require 'rubygems'
-require 'bundler'
-require 'rspec'
-require 'spork'
-require 'chef'
-
-Spork.prefork do
-  Dir[File.join(File.expand_path('../spec/support/**/*.rb', __dir__))].each { |f| require f }
-
-  RSpec.configure do |config|
-    config.expect_with :rspec do |c|
-      c.syntax = :expect
-    end
-
-    config.mock_with :rspec
-    config.treat_symbols_as_metadata_keys_with_true_values = true
-    config.filter_run focus: true
-    config.run_all_when_everything_filtered = true
-
-    config.before(:each) do
-      Chef::Config[:file_cache_path] = File.join(tmp_path, 'chef_cache')
-      FileUtils.rm_rf(tmp_path)
-      FileUtils.mkdir_p(tmp_path)
-      FileUtils.mkdir_p(Chef::Config[:file_cache_path])
-    end
-  end
-
-  def tmp_path
-    File.expand_path(File.join(File.dirname(__FILE__), '../tmp'))
-  end
-end
-
-Spork.each_run do
-  Dir["#{File.expand_path('..', File.dirname(__FILE__))}/libraries/*.rb"].sort.each do |path|
-    require_relative "../libraries/#{File.basename(path, '.rb')}"
-  end
-end
+# require 'rubygems'
+# require 'bundler'
+# require 'rspec'
+# require 'spork'
+# require 'chef'
+#
+# Spork.prefork do
+#   Dir[File.join(File.expand_path('../spec/support/**/*.rb', __dir__))].each { |f| require f }
+#
+#   RSpec.configure do |config|
+#     config.expect_with :rspec do |c|
+#       c.syntax = :expect
+#     end
+#
+#     config.mock_with :rspec
+#     config.treat_symbols_as_metadata_keys_with_true_values = true
+#     config.filter_run focus: true
+#     config.run_all_when_everything_filtered = true
+#
+#     config.before(:each) do
+#       Chef::Config[:file_cache_path] = File.join(tmp_path, 'chef_cache')
+#       FileUtils.rm_rf(tmp_path)
+#       FileUtils.mkdir_p(tmp_path)
+#       FileUtils.mkdir_p(Chef::Config[:file_cache_path])
+#     end
+#   end
+#
+#   def tmp_path
+#     File.expand_path(File.join(File.dirname(__FILE__), '../tmp'))
+#   end
+# end
+#
+# Spork.each_run do
+#   Dir["#{File.expand_path('..', File.dirname(__FILE__))}/libraries/*.rb"].sort.each do |path|
+#     require_relative "../libraries/#{File.basename(path, '.rb')}"
+#   end
+# end
